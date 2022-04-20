b6df6d51f549aaf500d7b081622f5dea0584c68c
Rohan Yadav <yadavrohan120@gmail.com>
2022-04-20 10:55:59 +0530

    Added a GUI

--- Bike_Control_Simulation/Bike_Control_Simulation.ino
+++ Bike_Control_Simulation/Bike_Control_Simulation.ino
@@ -5,14 +5,22 @@
 #define WHEEL 11
 #define BUZZER 10*/
 
-#define KEY 2
+/*#define KEY 2
 #define DN 6
 #define UP 7
 #define ENGINE 3
 #define WHEEL 5
-#define BUZZER 4
+#define BUZZER 4*/
+
+#define KEY 8
+#define DN 12
+#define UP 9
+#define ENGINE 11
+#define BUZZER 10
+
 #define HELMET_ON 0x3F
 #define HELMET_OFF 0xC0
+
 struct {
   bool up = 0;
   bool key = 0;
@@ -20,11 +28,13 @@ struct {
   bool engine = 0;
   bool helmet = 0;
   bool buzzer = 0;
+  bool throttle = 0;
 }stat;
 
+char buff[100];
+uint8_t Status = 0x00;
 uint8_t gear_index = 0;
 uint8_t gears[6] = {0,42,84,126,168,210};
-char buff[100];
 
 void Check_Vehicle_Status(){
   
@@ -40,12 +50,14 @@ void Check_Vehicle_Status(){
   if(!digitalRead(UP)){
     delay(1);
     if(!digitalRead(UP)){stat.up = 1;}
+    else{stat.up = 0;}
   }
 
   //check gear DOWN button status
   if(!digitalRead(DN)){
     delay(1);
     if(!digitalRead(DN)){stat.down = 1;}
+    else{stat.down = 0;}
   }
 
   //check healmet status
@@ -83,7 +95,7 @@ void setup(){
   digitalWrite(A1,LOW);
   digitalWrite(A2,LOW);
   
-  pinMode(WHEEL, OUTPUT);
+  //pinMode(WHEEL, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   pinMode(ENGINE, OUTPUT);
   pinMode(UP,INPUT_PULLUP);
@@ -96,13 +108,14 @@ void loop(){
   
   if(stat.helmet){
     stat.buzzer = 0;
+    stat.throttle = 0;
     Change_Gear_Normal();
     stat.engine = stat.key;
   }
   
   else{
     stat.buzzer = 1;
-    if(stat.engine){Change_Gear_Trotteled();}
+    if(stat.engine){stat.throttle = 1;Change_Gear_Trotteled();}
     else{stat.engine = 0;stat.buzzer = 0;}
   }
 
@@ -112,8 +125,13 @@ void loop(){
 
   digitalWrite(BUZZER,stat.buzzer);
   digitalWrite(ENGINE,stat.engine);
-  analogWrite(WHEEL,gears[gear_index]);
-  
+  //analogWrite(WHEEL,gears[gear_index]);
+
+  bitWrite(Status,7,stat.throttle);
+  bitWrite(Status,6,stat.engine);
+  bitWrite(Status,5,stat.key);
+  Status = (Status & 0xF0) | gear_index;
+  Serial.write(Status);
   //DEBUGING
   /*sprintf(buff,"UP_BUTTON: %d",stat.up);
   Serial.println(buff);
@@ -130,6 +148,5 @@ void loop(){
   sprintf(buff,"PWM: %d",gears[gear_index]);
   Serial.println(buff);
   Serial.println();*/
-  
   delay(125);
 }
--- webcam_helmet_detect.py
+++ webcam_helmet_detect.py
@@ -1,5 +1,7 @@
 from os.path import dirname, join
 import numpy as np
+import tkinter
+from PIL import ImageTk,Image
 import cv2
 from tensorflow import keras
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
@@ -9,31 +11,46 @@ import serial
 
 vid = cv2.VideoCapture(0)
 
+window = tkinter.Tk()
+window.title("Motor Cycle Monitor")
+window.geometry('880x480')
+
+Image_Label = tkinter.Label(window)
+Image_Label.place(x=0,y=0)
+
+MC_satate = tkinter.StringVar()
+MC_state_lable = tkinter.Label(window,textvariable=MC_satate,font=("Arial",25),justify="left")
+MC_state_lable.place(x=655,y=10)
+
 #creating dnn network for face detection
 protoPath = join(dirname(__file__), "deploy.prototxt.txt")
 modelPath = join(dirname(__file__), "res10_300x300_ssd_iter_140000.caffemodel")
 helmeth5 = join(dirname(__file__), "helmet.h5")
 network = cv2.dnn.readNetFromCaffe(protoPath,modelPath)
 model=keras.models.load_model(helmeth5)
-#network = cv2.dnn.readNetFromCaffe('deploy.prototxt.txt','res10_300x300_ssd_iter_140000.caffemodel')
-#model=keras.models.load_model('helmet.h5')
 
-SerialConn = False
+toggle = 0
 ACK = 0x3F
 NACK = 0xC0
 ACK = ACK.to_bytes(1,'big')
 NACK = NACK.to_bytes(1,'big')
-toggle = 0
+SerialConn = False
+THROTTEL = "OFF"
+HELMET = "OFF"
+ENGINE = "OFF"
+GEAR = 0
+KEY = "OFF"
+run = True
 
-while True:
+while run:
 
     if SerialConn == False:
         SerialConn = True
         try:
-            ArduinoSerial = serial.Serial('COM4',9600)
+            ArduinoSerial = serial.Serial(port='COM4',baudrate=9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=0.5)
         except:
             SerialConn = False
-        print(SerialConn)
+        if SerialConn: print(SerialConn)
     
     ret,frame = vid.read()
     if ret:
@@ -55,10 +72,11 @@ while True:
                 temp=preprocess_input(temp)
                 temp=np.expand_dims(temp,axis=0)
                 pred_val=model.predict(temp)
-                print(pred_val)
+                #print(pred_val)
                 pred_val=np.ravel(pred_val).item()
 
                 if pred_val < 0.7 :
+                    HELMET = "OFF"
                     text = 'NO-HELMET'+ str(pred_val)
                     cv2.rectangle(frame,(startX-100,startY-100),(endX+50,endY+50),(0,0,255),2)
                     cv2.putText(frame,text,(startX,Y),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3)
@@ -67,6 +85,7 @@ while True:
                         toggle = "NACK"
                         ArduinoSerial.write(NACK)
                 else:
+                    HELMET = "ON"
                     text = 'HELMET'+ str(pred_val)
                     cv2.rectangle(frame,(startX-100,startY-100),(endX+50,endY+50),(0,255,0),2)
                     cv2.putText(frame,text,(startX,Y),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3)
@@ -74,10 +93,43 @@ while True:
                         print("ACK")
                         toggle = "ACK"
                         ArduinoSerial.write(ACK)
+    
+    if SerialConn == True:
+        byte = ArduinoSerial.read()
+        try:
+            byte = int.from_bytes(byte,"big")
+        except:
+            byte=0x00
+    
+        THROTTEL = "OFF"
+        ENGINE = "OFF"
+        KEY = "OFF"
 
-        cv2.imshow('E-Tech Helmet Detection',frame)
-        cv2.waitKey(10)
-        if cv2.waitKey(1) == ord('e'):
-            break
+        if byte & 0x80:
+            THROTTEL = "ON"
+        if byte & 0x40:
+            ENGINE = "ON"
+        if byte & 0x20:
+            KEY = "ON"
+        
+        GEAR = 0x0F & byte
+    
+    try:
+        txt = "Throttle: {}\n\nHelmet: {}\n\nEngine: {}\n\nGear: {}\n\nKey: {}".format(THROTTEL,HELMET,ENGINE,GEAR,KEY)
+        MC_satate.set(txt)
+        Image_temp = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        Image_temp = ImageTk.PhotoImage(Image.fromarray(Image_temp))
+        Image_Label['image'] = Image_temp
+        window.update()
+    except:
+        run = False
+    
 ArduinoSerial.close()
+vid.release()
 cv2.destroyAllWindows()
+
+#                x x x x  x x x x
+#                ^ ^ ^    ^ ^ ^ ^
+#     Throttel___| | |    |_|_|_|___Gear count
+#         Engine___| |
+#              Key___|
